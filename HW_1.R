01188910f0fc45f611a47b612483548629ce34c0
Laura Cortes <lcortes251@gmail.com>
2018-09-03 16:03:57 -0400

    HW 2
    Describe three different forms of normalization for microarrays

---
+++ HW_1.R
@@ -0,0 +1,3 @@
+#The KM survival time tells you the probability of survival over time. The y-axis has the cumulative survival rate which takes into account the people that remain at risk, and the x-axis is time passed (since start of study for example). Thus the y-intercept can tell you the time as the cumulative survival changes.  The x-intercept can tell you the cumulative survival rate within a specified time. 
+ 
+#You need to use survObj to tell the program the response variable (the follow-up time and whether there was death of each patient, eg censorship and its direction). 
